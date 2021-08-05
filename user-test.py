f5462160c0daf036b02f83bf029e8d445418e8d3
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-08-05 12:46:05 +0300

    add changes in user-test and user

--- user-test.py
+++ user-test.py
@@ -9,4 +9,14 @@ class TestClass(unittest.TestCase):
         def test_init(self):
                 self.assertEqual =(self.newName,"Barry")
                 self.assertEqual =(self.newName,"Barry@1234")
+        def test_save_user(self):
+                self.new_user.save_user()
+                self.assertEqual (len(user.user_list),1)
+class TestClass(unittest.TestCase):
+        def setUp(self):
+                self.assertEqual = (self.new_credentials.user,"Barry")
+                self.assertEqual = (self.new_credentials.account,"email")
+                self.assertEqual = (self.new_credentials.password,"Barry@1234")
+	   
+                           
 	    
--- user.py
+++ user.py
@@ -7,4 +7,11 @@ class TestClass(unittest.TestCase):
         def setUp(self):
 	    #method used to run the test
 	           self.new_user("Barry","Barry@123")
+        def test_init(self):
+                self.assertEqual = (self.new_user.userName, "Barry")
+                self.assertEqual = (self.new_userPassword,"Barry@1234")
+        def test_save_user(self):
+                self.new_user.save_user()
+                self.assertEqual (len(user.user_list),1)
+
         
